5d21df654bfc784324d5ee2f1f21319f8ddf2401
Carolyehhh <carolyehhh@gmail.com>
2024-12-10 09:06:07 +0800

    解決殘差偏性，但模型預測效果下滑，故捨棄

--- Market_Transaction_OLS_Prediction.py
+++ Market_Transaction_OLS_Prediction.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
+from scipy.stats import boxcox
 from Module import Connect_to_MSSQL, extract_data, get_last_month_of_quarter
 from data_SQLquery_list import MacroData
 
@@ -54,23 +56,89 @@ pivot_MacroVar.reset_index(inplace=True)
 
 pivot_MacroVar['年季'] = pivot_MacroVar['年月'].apply(get_last_month_of_quarter)
 Combined_df = pd.merge(Combined_df, pivot_MacroVar[['年季','經濟成長率(GDP)–單季', '國內生產毛額(GDP)–美元', '平均每人國內生產毛額(GDP)–美元']].dropna(), how='left', on=['年季', '年季'])
+# print(len(Combined_df)) #94
 
-# 確認回歸模型的前提條件
 # 母體資料的線性關係
-# 相關性
 X = Combined_df[['是否放假', '經濟成長率(GDP)–單季', '國內生產毛額(GDP)–美元',
        '平均每人國內生產毛額(GDP)–美元', '消費者物價指數(CPI)',
        '消費者物價指數(CPI)年增率', '貨幣總額(M1B)日平均', '貨幣總額年增率(M1B)期底', '貨幣總額(M2)日平均',
        '貨幣總額年增率(M2)期底', '中央銀行重貼現率', '失業率', '交易天數_月', '上個月成交金額(千)_月',
        '上上個月成交金額(千)_月']]
+
+# 添加截距項，當自變數都為0時，Y的期望值 => 提升模型表現力(更好地擬合)、正確解釋模型參數、兼容性(符合標準的回歸公式)
+X = sm.add_constant(X)
+
 Y = Combined_df['成交金額(千)_月']
-correlation_matrix = X.corrwith(Y)
-# print(correlation_matrix)
 
 # OLS Model
 transaction_amount_model0 = sm.OLS(Y, X)
 result0 = transaction_amount_model0.fit()
 print(result0.summary())
+
+# # 解決殘差非常態分佈的問題: 
+# # 偏度計算
+# skewness_values = Combined_df.select_dtypes(include='number').apply(lambda x: stats.skew(x, bias=False))
+# # print(skewness_values)
+# # 針對偏度大的變數優先處理(>1、<-1)
+# # 右偏，取對數處理
+# high_skewness = skewness_values[skewness_values > 1]
+# for col in high_skewness.index:
+#     Combined_df[col + '_log'] = np.log1p(Combined_df[col]) # Log(1+x) 避免 Log(0)
+# # print("High", high_skewness)
+# # 左偏，平方根處理
+# low_skewness = skewness_values[skewness_values < -1]
+# for col in low_skewness.index:
+#     Combined_df[col + '_sqrt'] = np.sqrt(Combined_df[col])
+# # print("Low", low_skewness)
+
+# # 取對數&平方根處理後，不偏性沒有明顯的改善
+# filter_columns = Combined_df.filter(like='_sqrt').columns.tolist() + Combined_df.filter(like='_log').columns.tolist()
+# skewness_values2 = Combined_df[filter_columns].apply(lambda x: stats.skew(x, bias=False))
+# # print("transform", skewness_values2)
+
+# # 針對偏性仍很大的變數進行 Box-Cox
+# Combined_df['交易天數_月_boxcox'], _ = boxcox(Combined_df['交易天數_月'])
+# Combined_df['失業率_boxcox'], _ = boxcox(Combined_df['失業率'])
+
+# # delete useless columns
+# Combined_df = Combined_df.drop(['交易天數_月_sqrt', '失業率_log'], axis=1)
+# # print(Combined_df.columns)
+
+# # # 計算變換後的偏度
+# # skewness_v = stats.skew(Combined_df['交易天數_月_boxcox'], bias=False) # -0.2446
+# # skewness_vv = stats.skew(Combined_df['失業率_boxcox'], bias=False) # -0.0685
+# # print(f"Box-Cox後的偏度: {skewness_vv}")
+
+# # 使用解決過"偏性"的變數做回歸
+# X1 = Combined_df[['是否放假_log', '經濟成長率(GDP)–單季', '國內生產毛額(GDP)–美元',
+#        '平均每人國內生產毛額(GDP)–美元', '消費者物價指數(CPI)',
+#        '消費者物價指數(CPI)年增率', '貨幣總額(M1B)日平均', '貨幣總額年增率(M1B)期底_log', '貨幣總額(M2)日平均',
+#        '貨幣總額年增率(M2)期底', '中央銀行重貼現率', '失業率_boxcox', '交易天數_月_boxcox', '上個月成交金額(千)_月',
+#        '上上個月成交金額(千)_月']]
+
+# # 添加截距項，當自變數都為0時，Y的期望值 => 提升模型表現力(更好地擬合)、正確解釋模型參數、兼容性(符合標準的回歸公式)
+# X1 = sm.add_constant(X1)
+
+# Y1 = Combined_df['成交金額(千)_月']
+
+# # OLS Model
+# transaction_amount_model1 = sm.OLS(Y1, X1)
+# result1 = transaction_amount_model1.fit()
+# print(result1.summary())
+# # 結論：基於我的目的是"預測"市場金額，殘差的"偏性"可能對於預測結果影響不大，加上使用處理過的變數導致殘差變得更非常態分佈，故先不處理這部分
+
+
+# # 對Y(市場成交金額)預估
+# Y_pred_train = result0.predict(X)
+# # print(Y_pred_train)
+
+# 比較:預測值 vs 實際值
+# actual_vs_predicted = pd.DataFrame({
+#     "Actual":Y,
+#     "Predicted":Y_pred_train,
+#     "Difference": Y_pred_train - Y
+# })
+# print(len(actual_vs_predicted)) #94
 
 # 殘差項
 # A.常態分佈
@@ -81,14 +149,8 @@ print(result0.summary())
 
 
 
-
-
 # =================
-# #duncan_prestige = sm.datasets.get_rdataset("Duncan", "carData")
-# #Y = duncan_prestige.data['income']
-# #X = duncan_prestige.data['education']
-# #x = sm.add_constant(X)
-
+# 建文原使的 code
 # df=pd.read_excel("成交金額預估.xlsx",sheet_name="model_4")
 # Y=df["成交億"]
 # X=df.iloc[:,2:23]
@@ -140,3 +202,9 @@ print(result0.summary())
 # L1: 上個月的市場成交金額 V
 # L2: 上上個月的市場成交金額 V
 # 市場上的交易日 V
+
+#====================================
+# # 檢查資料的分布形狀
+# sns.histplot(Combined_df['失業率'], kde=True)
+# plt.title('Histogram of unemployee rate')
+# plt.show()
